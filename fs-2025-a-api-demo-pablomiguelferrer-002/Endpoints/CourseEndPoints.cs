3611d6ee1c5d82b68108c4787214f4c5beaf6835
Pablo <pmiguelferrer@gmail.com>
2025-09-25 11:56:46 +0100

    change coursendpoints

--- fs-2025-a-api-demo-pablomiguelferrer-002/Endpoints/CourseEndPoints.cs
+++ fs-2025-a-api-demo-pablomiguelferrer-002/Endpoints/CourseEndPoints.cs
@@ -5,16 +5,40 @@ namespace fs_2025_a_api_demo_pablomiguelferrer_002
     {
         public static void AddCourseEndPoint(this WebApplication app)
         {
-            app.MapGet("/courses", (CourseData courseData) =>
-            {
-                return Results.Ok(courseData.Courses);
-            });
+            app.MapGet("/courses", LoadAllCoursesAsync);
 
             app.MapGet("/courses/{id:int}", (int id, CourseData courseData) =>
             {
                 var course = courseData.Courses.FirstOrDefault(c => c.Id == id);
                 return course is not null ? Results.Ok(course) : Results.NotFound();
             });
+        }
+
+        private static async Task<IResult> LoadCourseById(CourseData courseData, int id)
+        {
+            var output = courseData.Courses.FirstOrDefault(c => c.Id == id);
+            if (output is null)
+            {
+                return Results.NotFound();
+            }
+            return Results.Ok(output);
+
+        }
+
+        private static async Task<IResult> LoadAllCoursesAsync(CourseData courseData, string? courseType, string? search)
+        {
+            var output = courseData.Courses;
+            if (!string.IsNullOrWhiteSpace(courseType))
+            {
+                output = output.Where(c => c.courseType.Equals(courseType, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                output = output.Where(c => c.courseName.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                                           c.shortDescription.Contains(search, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+            return Results.Ok(output);
         }
     }
 }
