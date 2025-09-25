dafaaf6306fc79dd9f8d85fdc84dab3a43457e55
Pablo <pmiguelferrer@gmail.com>
2025-09-25 11:25:00 +0100

    add bookData, book json, book endpoint, book model, and add dependencies

--- fs-2025-a-api-demo-pablomiguelferrer-002/Models/Book.cs
+++ fs-2025-a-api-demo-pablomiguelferrer-002/Models/Book.cs
@@ -2,5 +2,11 @@
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string isbn { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string publication_date { get; set; }
+        public string genre { get; set; }
     }
 }
--- fs-2025-a-api-demo-pablomiguelferrer-002/Startup/DependenciesConfig.cs
+++ fs-2025-a-api-demo-pablomiguelferrer-002/Startup/DependenciesConfig.cs
@@ -7,6 +7,7 @@ namespace fs_2025_a_api_demo_pablomiguelferrer_002.Startup
         public static void AddDependencies(this WebApplicationBuilder builder)
         {
             builder.Services.AddTransient<CourseData>();
+            builder.Services.AddTransient<BookData>();
         }
     }
 }
